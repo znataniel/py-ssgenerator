33c75f0e78291d415e4157d2b0e7eaca02aed641
znataniel <znataniel@protonmail.com>
2024-03-14 17:10:42 -0300

    cleanup

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,3 +25,15 @@ class HTMLNode:
         else:
             children_str = self.children
         return f"HTMLNode\ntag: {self.tag}\nvalue: {self.value}\nchildren:{children_str}\nprops: {self.props_to_html()}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError()
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -28,6 +28,29 @@ class TestHTMLNode(unittest.TestCase):
             "a", "GNU Webpage", None, {"href": "gnu.org", "target": "_blank"}
         )
         self.assertRaises(NotImplementedError, hnode_1.to_html)
+
+    if __name__ == "__main__":
+        unittest.main()
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        l_node = LeafNode("p", "This is a para.", {"style": "margin-top: 1rem"})
+        expected = '<p style="margin-top: 1rem">This is a para.</p>'
+        self.assertEqual(l_node.to_html(), expected)
+
+    def test_to_html_no_props(self):
+        l_node = LeafNode("p", "This is a para.")
+        expected = "<p>This is a para.</p>"
+        self.assertEqual(l_node.to_html(), expected)
+
+    def test_to_html_no_vals(self):
+        l_node = LeafNode("p", None)
+        self.assertRaises(ValueError, l_node.to_html)
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
     if __name__ == "__main__":
         unittest.main()
