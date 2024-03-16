4f9c70be8543152d0510cc6db4ae7a4df5a34c8b
znataniel <znataniel@protonmail.com>
2024-03-16 14:34:33 -0300

    refactor split with delimiter. Add text_to_textnode function + some tests

--- src/inline_md_parsing.py
+++ src/inline_md_parsing.py
@@ -1,36 +1,30 @@
-from textnode import TextNode, text_type_text, text_type_image, text_type_link
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_image,
+    text_type_link,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+)
 import re
 
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter, text_type):
     nodes_split = []
     for on in old_nodes:
-        if delimiter not in on.text or on.text_type != text_type_text:
+        if on.text_type != text_type_text:
             nodes_split.append(on)
             continue
-
-        aux_split = on.text.split(delimiter, maxsplit=1)
-        nodes_split.append(TextNode(aux_split[0], on.text_type))
-        if delimiter not in aux_split[1]:
+        splits = on.text.split(delimiter)
+        if not len(splits) % 2:
             raise Exception(f"Invalid markdown syntax: closing {delimiter} not found")
-
-        delimiter_has_closed = 1
-        while delimiter in aux_split[1]:
-            aux_split = aux_split[1].split(delimiter, maxsplit=1)
-            if aux_split[0]:
-                nodes_split.append(
-                    TextNode(
-                        aux_split[0],
-                        text_type if delimiter_has_closed % 2 else text_type_text,
-                    )
-                )
-            delimiter_has_closed += 1
-
-        if delimiter_has_closed % 2:
-            raise Exception(f"Invalid markdown syntax: closing {delimiter} not found")
-
-        if aux_split[1]:
-            nodes_split.append(TextNode(aux_split[1], on.text_type))
+        for i in range(len(splits)):
+            if not splits[i]:
+                continue
+            nodes_split.append(
+                TextNode(splits[i], text_type if i % 2 else text_type_text)
+            )
     return nodes_split
 
 
@@ -78,3 +72,12 @@ def split_nodes_link(old_nodes: list[TextNode]):
         if on.text:
             nodes_split.append(on)
     return nodes_split
+
+
+def text_to_textnodes(text: str):
+    split_imgs = split_nodes_image([TextNode(text, text_type_text)])
+    split_links = split_nodes_link(split_imgs)
+    split_bold = split_nodes_delimiter(split_links, "**", text_type_bold)
+    split_italic = split_nodes_delimiter(split_bold, "*", text_type_italic)
+    split_code = split_nodes_delimiter(split_italic, "`", text_type_code)
+    return split_code
