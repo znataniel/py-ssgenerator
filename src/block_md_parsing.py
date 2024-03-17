874e3a6e2a4dd8d6600dacaae2212b1f23b1fcce
znataniel <znataniel@protonmail.com>
2024-03-17 13:02:33 -0300

    Add function to detect markdown block types + tests

--- src/block_md_parsing.py
+++ src/block_md_parsing.py
@@ -1,4 +1,45 @@
+import re
+
+
 def markdown_to_blocks(markdown: str):
     splits = markdown.split("\n\n")
     splits_stripped = list(map(lambda block: block.strip("\n "), splits))
     return list(filter(lambda block: block != "" and block != "\n", splits_stripped))
+
+
+block_type_para = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_ul = "unordered_list"
+block_type_ol = "ordered_list"
+
+
+def block_to_block_type(block: str):
+    heading_pattern = r"\#{1,6} \w+"
+    if re.match(heading_pattern, block):
+        return block_type_heading
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+    lines_split = block.split("\n")
+    if block.startswith(">"):
+        if all([line.startswith(">") for line in lines_split]):
+            return block_type_quote
+        return block_type_para
+    if block.startswith("* "):
+        if all([line.startswith("* ") for line in lines_split]):
+            return block_type_ul
+        return block_type_para
+    if block.startswith("- "):
+        if all([line.startswith("- ") for line in lines_split]):
+            return block_type_ul
+        return block_type_para
+    if block.startswith("1. "):
+        if all(
+            [
+                lines_split[i - 1].startswith(f"{i}. ")
+                for i in range(2, len(lines_split) + 1)
+            ]
+        ):
+            return block_type_ol
+    return block_type_para
--- src/test_block_md_parsing.py
+++ src/test_block_md_parsing.py
@@ -1,6 +1,13 @@
 import unittest
 from block_md_parsing import (
     markdown_to_blocks,
+    block_to_block_type,
+    block_type_para,
+    block_type_heading,
+    block_type_code,
+    block_type_quote,
+    block_type_ul,
+    block_type_ol,
 )
 
 
@@ -28,3 +35,128 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
 
     if __name__ == "__main__":
         unittest.main()
+
+
+class TestBlockType(unittest.TestCase):
+    def test_heading(self):
+        blocks = [
+            "# Heading",
+            "## Heading",
+            "### Heading",
+            "#### Heading",
+            "##### Heading",
+            "###### Heading",
+            "########## Heading",
+            "####Heading",
+        ]
+        expected = [
+            block_type_heading,
+            block_type_heading,
+            block_type_heading,
+            block_type_heading,
+            block_type_heading,
+            block_type_heading,
+            block_type_para,
+            block_type_para,
+        ]
+        self.assertEqual(expected, [block_to_block_type(bl) for bl in blocks])
+
+    def test_code(self):
+        blocks = [
+            """```
+            This is code
+                ```""",
+            """```This is code also
+                ```""",
+            """```
+                This is code too```""",
+            "```We all code```",
+            """``
+                This ain't code
+                ```""",
+            """```
+                This ain't codeeither
+                `""",
+        ]
+        expected = [
+            block_type_code,
+            block_type_code,
+            block_type_code,
+            block_type_code,
+            block_type_para,
+            block_type_para,
+        ]
+        self.assertEqual(expected, [block_to_block_type(bl) for bl in blocks])
+
+    def test_quote(self):
+        blocks = [
+            "> quote",
+            """> Many quotes
+> Many quotes
+> Many quotes""",
+            """> quote
+> quote
+>quote""",
+            """> quote
+> quote
+# Not a quote""",
+        ]
+        expected = [
+            block_type_quote,
+            block_type_quote,
+            block_type_quote,
+            block_type_para,
+        ]
+        self.assertEqual(expected, [block_to_block_type(bl) for bl in blocks])
+
+    if __name__ == "__main__":
+        unittest.main()
+
+    def test_ul(self):
+        blocks = [
+            "* single element",
+            "- single element",
+            "*- not an element",
+            """* element
+* element
+* element""",
+            """- element
+- element
+- element""",
+            """* element
+- element
+- element""",
+            """* element
+element
+- element""",
+        ]
+        expected = [
+            block_type_ul,
+            block_type_ul,
+            block_type_para,
+            block_type_ul,
+            block_type_ul,
+            block_type_para,
+            block_type_para,
+        ]
+        self.assertEqual(expected, [block_to_block_type(bl) for bl in blocks])
+
+    def test_ol(self):
+        blocks = [
+            "1. li",
+            "1. li\n2. li\n3. li",
+            "1. li\n4. li\n3. li",
+            "1. li\n* li\n3. li",
+        ]
+        expected = [
+            block_type_ol,
+            block_type_ol,
+            block_type_para,
+            block_type_para,
+        ]
+        self.assertEqual(expected, [block_to_block_type(bl) for bl in blocks])
+
+    def test_para(self):
+        blocks = ["Lorem ipsum sit amet, qui adipisicing minim sint cupidatat."]
+        expected = [block_type_para]
+        self.assertEqual(expected, [block_to_block_type(bl) for bl in blocks])
